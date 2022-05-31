d79fed88b7c3f9cc61bc1318c6ceb5cf71bb9850
SALLU00125 <88003389+SALLU00125@users.noreply.github.com>
2022-06-01 02:02:49 +0500

    INITIAL COMMIT

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <NavigationContainer>
+        <Stack.Navigator>
+             <Stack.Screen
+          name="Home"
+         
+          options={{ title: 'Welcome' }}
+        />
+
+        <Stack.Screen name="Profile" />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+   
   );
 }
 
